4c796cb23e2c862a849d49146f939ffc969cfdcd
Anshul Kumar <105858846+anshul393@users.noreply.github.com>
2023-02-01 11:24:39 +0000

    Updating cli-functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,8 @@ def wiki(name="War Goddess", length=1):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_name(name):
+    "This is a wikipedia name searcher"
+
+    return wikipedia.search(name)
